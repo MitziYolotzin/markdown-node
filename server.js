2ad57f3cf139f1050d6e187adf7539eb46bd953a
MitziYolotzin <mitziulter@gmail.com>
2020-03-18 17:56:55 -0600

    create view html

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 
+//ejs to html
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    //res.send('Hello world')
+    res.render('index')
 })
 
 app.listen(5000)
